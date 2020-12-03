3950de58dc0e7bb4a3e400193dcce80e72400f79
Sullivan Young <sullivangyoung@gmail.com>
2020-12-03 16:58:53 -0500

    cms preparation edits

---
+++ PortfolioSite/Models/ProjectPortfolio.cs
@@ -0,0 +1,25 @@
+﻿using DocumentFormat.OpenXml.Spreadsheet;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PortfolioSite.Models
+{
+    public class ProjectPortfolio
+    {
+        //[TextField]
+        //public string Title { get; set; }
+
+        //[MultimediaField]
+        //public IMultimedia picture { get; set; }
+
+        //[TextField]
+
+        //public string body { get; set; }
+
+        //[LinkedComponentField]
+        //public ExternalLink githubLink { get; set; }
+    }
+}
